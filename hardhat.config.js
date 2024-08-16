abb98ab63920a388843ea2b42e5a5249987f76a4
Tungneo <66679360+worms12345656@users.noreply.github.com>
2024-08-16 21:24:19 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      //2nd wallet
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
